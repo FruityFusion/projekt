fdaf958a13fdbbd1f52110ac2c5e44e67546d84f
FruityFusion <dominik.baelz@googlemail.com>
2016-10-20 11:45:55 +0200

    PHP Backend hinzugefügt

--- kosten.php
+++ kosten.php
@@ -1,3 +1,18 @@
 <?php
-	echo $_GET['gesendet'];
+	/* Author: Dominik Bälz
+	 * Last: 20/10/2016
+	 * Description: Contains the main logic (Procedural Style)
+	*/
+	
+	echo $_GET['fst'];
+	echo $_GET['sth'];
+	echo $_GET['ast'];
+	echo $_GET['baujahr'];
+	
+	$handle = fopen("kosten.csv","w+");
+	$data = '"Fläche je Stockwerk in QM";"Anzahl der Stockwerke";"Baujahr";'."\n";
+	$data .= '"'. $_GET['fst'] . '";' . $_GET['sth'] . '";' . $_GET['ast'] . '";' . $_GET['baujahr'] . '";' .  "\n";
+	fwrite($handle,$data);
+	fclose($handle);
+	
 ?>
